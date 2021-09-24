f70463a85a09e734ef407fa14af51ddf7c35ee82
Natale <nataleconsalvi06@gmail.com>
2021-09-24 16:17:09 +0200

    end BasicStack

--- main.cpp
+++ main.cpp
@@ -4,7 +4,89 @@
 
 using namespace std;
 
+/*
 int main() {
+    BasicStack bs = BasicStack();
+
+    cout << bs.size() << endl;
+
+    bs.push('a');
+    bs.push('b');
+    bs.push('d');
+    cout << bs.as_string() << endl;
+    cout << bs.peek() << endl;
+    cout << bs.as_string() << endl;
+    cout << bs.pop() << endl;
+    cout << bs.as_string() << endl;
+    bs.push('e');
+    cout << bs.as_string() << endl;
+
+    cout << bs.peekn(2) << endl;
 
     return 0;
+}
+*/
+
+int main () {
+  
+   cout << endl;
+   cout << "-----  BASIC STACK" << endl;   
+ 
+   BasicStack bs = BasicStack();
+ 
+   Stack* ps1 = &bs;
+   cout << ps1->as_string() << endl;  // BasicStack: elements=
+   ps1->push('a');
+   cout << ps1->as_string() << endl;  // BasicStack: elements=a
+   cout << ps1->peek() << endl;       // 'a'
+   ps1->push('b');
+   cout << ps1->peek() << endl;       // 'b'
+   cout << ps1->as_string() << endl;  // BasicStack: elements=ab
+   cout << ps1->peek() << endl;       // 'b'
+   ps1->push('c');                   
+   cout << ps1->peek() << endl;       // 'c'
+   cout << ps1->as_string() << endl;  // BasicStack: elements=abc
+   cout << ps1->pop() << endl;        // 'c'
+   cout << ps1->pop() << endl;        // 'b'
+   cout << ps1->as_string() << endl;  // BasicStack: elements=a
+   cout << ps1->pop() << endl;        // 'a'
+  
+   try {
+       ps1->pop(); //  can't pop empty stack
+   } catch (std::runtime_error& ex) {
+       cout << "something bad happened!" << ex.what() << endl;
+       cout << "I caught the exception, will continue" << endl;
+   };
+ 
+   try {
+       ps1->peek(); // can't peek empty stack
+   } catch (std::runtime_error& ex) {
+       cout << "something bad happened!" << ex.what() << endl;
+       cout << "I caught the exception, will continue" << endl;
+   };
+ 
+   cout << endl;
+   cout << "-----  RESETTING BASIC STACK" << endl;   
+   bs = BasicStack();
+   ps1->push('a');
+   ps1->push('b');
+   ps1->push('c');
+   ps1->push('d');
+   ps1->push('e');
+   cout << ps1->as_string() << endl;  // abcde
+   cout << ps1->peekn(3) << endl;     // cde
+   cout << ps1->peekn(5) << endl;     // abcde
+   try {
+       cout << ps1->peekn(6) << endl;    // error
+   } catch (std::invalid_argument& ex) {
+       cout << "something bad happened!" << ex.what() << endl;
+       cout << "I caught the exception, will continue" << endl;
+   };
+ 
+   cout << ps1->as_string() << endl;  // abcde
+   cout << ps1->popn(3) << endl;      // cde
+   cout << ps1->as_string() << endl;  // ab
+ 
+ 
+   cout << endl;
 }
--- src/BasicStack.cpp
+++ src/BasicStack.cpp
@@ -1,1 +1,81 @@
 #include "basic_stack.h"
+
+void debug_stamp(std::string s){
+    cout << "DEBUG: " << s << "." << endl;
+}
+
+int BasicStack::size() const {
+    debug_stamp("analysing the size of stack");
+    return this->s.size();
+}
+
+std::string BasicStack::as_string() const {
+    debug_stamp("printing stack");
+    return string("BasicStack: ") + string("cap = 4 ") + string("elements: '") + this->s + "'";
+}
+
+bool BasicStack::is_empty() const {
+    debug_stamp("analysing if the stack is empty");
+    if(size() == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+void BasicStack::push(const char c) {
+    debug_stamp(string("adding element '") + c + string("' at the end of stack"));
+    this->s += c;
+}
+
+char BasicStack::peek() const {
+    debug_stamp("analysing the element at the end of stack");
+    if(this->s.size() == 0){
+        throw std::runtime_error(string("Stack is empty!"));
+    }else{
+        return this->s.back();
+    }
+    
+}
+
+char BasicStack::pop() {
+    debug_stamp("poppint the element at the end of stack");
+    if(this->s.size() == 0){
+        throw std::runtime_error(string("Stack is empty!"));
+    }else{
+        char c = this->s.back();
+        this->s.pop_back();
+        return c;
+    }
+}
+
+string BasicStack::peekn(const int n) const {
+
+    debug_stamp(string("analysing the last ") + std::to_string(n) + string(" elements at the end of stack"));
+    if(n < 0){
+        throw std::invalid_argument(string("n is negative!"));
+    }
+    if(n > this->s.size()){
+        throw std::invalid_argument(string("n is greater than size of stack! n = ") + std::to_string(n) + " > " + std::to_string(this->s.size()) + " (size)");
+    }
+
+    int i = this->s.size();
+    return this->s.substr(i - n, n);
+}
+
+string BasicStack::popn(const int n) {
+
+    debug_stamp(string("poppint the last ") + std::to_string(n) + string(" elements at the end of stack"));
+    if(n < 0){
+        throw std::invalid_argument(string("n is negative!"));
+    }
+    if(n > this->s.size()){
+        throw std::invalid_argument(string("n is greater than size of stack! (n) ") + std::to_string(n) + " > " + std::to_string(this->s.size()) + " (size)");
+    }
+    int i = this->s.size();
+    string a = this->s.substr(i - n, n);
+    for(int i = 0; i < n; i++){
+        this->s.pop_back();
+    }
+    return a;
+}
